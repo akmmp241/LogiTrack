b8d9ff824baa65621e241b20234e11d4aa5c5412
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2026-01-08 16:55:38 +0700

    feat(config): add config for rabbitmq

---
+++ libs/config/src/rabbitmq.rs
@@ -0,0 +1,25 @@
+use lapin::{Channel, Connection, ConnectionProperties};
+
+pub async fn get_connection() -> Result<Connection, lapin::Error> {
+    let user = std::env::var("RABBITMQ_USER").expect("RABBITMQ_USER not set");
+    let pass = std::env::var("RABBITMQ_PASSWORD").expect("RABBITMQ_PASSWORD not set");
+    let host = std::env::var("RABBITMQ_HOST").expect("RABBITMQ_HOST not set");
+    let port = std::env::var("RABBITMQ_PORT").expect("RABBITMQ_PORT not set");
+
+    let dsn = format!("amqp://{}:{}@{}:{}/%2f", user, pass, host, port);
+
+    let conn = Connection::connect(
+        dsn.as_str(),
+        ConnectionProperties::default(),
+    ).await?;
+
+    Ok(conn)
+}
+
+pub async fn create_channel() -> Result<Channel, lapin::Error> {
+    let conn = get_connection().await?;
+
+    let channel = (&conn).create_channel().await?;
+
+    Ok(channel)
+}
