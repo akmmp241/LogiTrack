233aefb76ec601a30e6569a1d9aed6fe97c81a3d
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2026-01-11 07:45:15 +0700

    feat(notification-service): implement email notification sender
    - setup service architecture, such as structure, channel abstraction, template folders.
    - implement email sender using lapin SMTP provider

---
+++ services/notification-service/src/ports/email.rs
@@ -0,0 +1,77 @@
+use crate::domain;
+use crate::domain::TrackingEventMsg;
+use crate::ports::ChannelPort;
+use anyhow::anyhow;
+use config::lettre::create_smtp_transport;
+use domain::{TemplateId, TrackingMsgPayload};
+use handlebars::Handlebars;
+use lettre::message::Mailbox;
+use lettre::message::header::ContentType;
+use lettre::{AsyncSmtpTransport, AsyncTransport, Message, Tokio1Executor};
+use std::env;
+
+pub struct EmailSmtpSender {
+    mailer: AsyncSmtpTransport<Tokio1Executor>,
+    from: Mailbox,
+}
+
+impl EmailSmtpSender {
+    pub async fn new() -> Self {
+        let mailer = create_smtp_transport()
+            .await
+            .expect("Failed to create smtp transport");
+
+        let from = env::var("SMTP_FROM_EMAIL").expect("SMTP_FROM_EMAIL must be set");
+
+        Self {
+            mailer,
+            from: from.parse().expect("Failed to parse SMTP_FROM_EMAIL"),
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl ChannelPort for EmailSmtpSender {
+    async fn send(
+        &self,
+        event: &TrackingEventMsg,
+        content: String,
+        subject: String,
+    ) -> anyhow::Result<()> {
+        let email = Message::builder()
+            .from(self.from.clone())
+            .to(event.recipient.parse()?)
+            .subject(subject.as_str())
+            .header(ContentType::TEXT_HTML)
+            .body(content)?;
+
+        self.mailer.send(email).await?;
+
+        Ok(())
+    }
+
+    fn render(
+        &self,
+        template_id: TemplateId,
+        data: &TrackingMsgPayload,
+    ) -> anyhow::Result<(String, String)> {
+        let mut handlebars = Handlebars::new();
+
+        let template = match template_id {
+            TemplateId::TrackingCreatedEmail => (
+                "templates/tracking_added/email.mustache",
+                "Your Shipment Is On Tracking",
+            ),
+            _ => return Err(anyhow!("invalid template")),
+        };
+
+        let root = std::env!("CARGO_MANIFEST_DIR");
+        let path = std::path::Path::new(root).join(template.0);
+
+        handlebars.register_template_file("email", path)?;
+
+        let rendered = handlebars.render("email", data)?;
+
+        Ok((rendered, template.1.into()))
+    }
+}
---
+++ services/notification-service/src/ports/whatsapp.rs
@@ -0,0 +1,37 @@
+use crate::domain::{TemplateId, TrackingEventMsg, TrackingMsgPayload};
+use crate::ports::ChannelPort;
+use config::reqwest::get_reqwest_pool;
+use reqwest::Client;
+
+pub struct WhatsappSender {
+    client: Client,
+}
+
+impl WhatsappSender {
+    pub fn new() -> Self {
+        let client = get_reqwest_pool().expect("Failed to create reqwest pool");
+
+        Self { client }
+    }
+}
+
+#[async_trait::async_trait]
+impl ChannelPort for WhatsappSender {
+    async fn send(
+        &self,
+        event: &TrackingEventMsg,
+        content: String,
+        subject: String,
+    ) -> anyhow::Result<()> {
+        tracing::info!("sending tracking event to whatsapp");
+        todo!()
+    }
+
+    fn render(
+        &self,
+        template_id: TemplateId,
+        data: &TrackingMsgPayload,
+    ) -> anyhow::Result<(String, String)> {
+        todo!()
+    }
+}
