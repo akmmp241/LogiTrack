dbea82c4cdd4d994f86420d32cb781b218acb93c
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2026-03-04 20:59:18 +0700

    libs: update observability tracing config

--- libs/observability/src/lib.rs
+++ libs/observability/src/lib.rs
@@ -1,7 +1,11 @@
-use tracing_subscriber::fmt;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{EnvFilter, fmt};
 
 pub fn init(service_name: &str) {
-    fmt()
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env())
+        .with(tracing_subscriber::fmt::layer())
         .init();
 
     tracing::info!("{} is initialized", service_name);
