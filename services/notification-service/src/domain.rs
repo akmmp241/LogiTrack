47ad7632679e2b28c59f89c4e25ca19b9e0cf011
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2026-03-05 12:00:56 +0700

    feat(notification-service): implement create notification log after sent

--- services/notification-service/src/domain.rs
+++ services/notification-service/src/domain.rs
@@ -1,8 +1,11 @@
 use serde::{Deserialize, Serialize};
+use sqlx::Type;
+use sqlx::types::chrono::{DateTime, Utc};
 use uuid::Uuid;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Type)]
 #[serde(rename_all = "UPPERCASE")]
+#[sqlx(type_name = "notification_channel")]
 pub enum NotificationChannel {
     Whatsapp,
     Email,
@@ -30,6 +33,7 @@ pub struct TrackingEventMsg {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct TrackingMsgPayload {
+    pub shipment_id: Uuid,
     pub waybill_id: String,
     pub status: String,
     pub courier: String,
@@ -43,4 +47,17 @@ pub enum TemplateId {
     TrackingStatusUpdatedEmail,
     TrackingStatusUpdatedWa,
     TrackingStatusUpdatedTele,
+}
+
+pub struct NotificationLog {
+    pub id: Uuid,
+    pub shipment_id: Uuid,
+    pub event_id: Uuid,
+    pub channel: NotificationChannel,
+    pub recipient_to: String,
+    pub message_content: String,
+    pub status: String,
+    pub error_message: String,
+    pub sent_at: DateTime<Utc>,
+    pub created_at: DateTime<Utc>,
 }
--- services/notification-service/src/handler.rs
+++ services/notification-service/src/handler.rs
@@ -1,14 +1,19 @@
-use crate::domain::{NotificationChannel, TemplateId, TrackingEventMsg, TrackingEventMsgType};
+use crate::domain::{
+    NotificationChannel, NotificationLog, TemplateId, TrackingEventMsg, TrackingEventMsgType,
+};
 use crate::ports::ChannelPort;
+use sqlx::PgPool;
+use sqlx::types::chrono::Utc;
 use std::sync::Arc;
 
 pub struct NotificationHandler {
     sender: Arc<dyn ChannelPort>,
+    pool: PgPool,
 }
 
 impl NotificationHandler {
-    pub async fn new(sender: Arc<dyn ChannelPort>) -> Self {
-        Self { sender }
+    pub async fn new(sender: Arc<dyn ChannelPort>, pool: PgPool) -> Self {
+        Self { sender, pool }
     }
 
     pub async fn handle(&self, event: &TrackingEventMsg) -> anyhow::Result<()> {
@@ -16,7 +21,21 @@ impl NotificationHandler {
 
         let (content, subject) = self.sender.render(template, &mut event.payload.clone())?;
 
-        self.sender.send(event, content, subject).await?;
+        self.sender.send(event, content.clone(), subject).await?;
+
+        self.create_notification_log(&NotificationLog {
+            id: event.message_id,
+            shipment_id: event.payload.shipment_id,
+            event_id: event.message_id,
+            channel: event.channel.clone(),
+            recipient_to: event.recipient.clone(),
+            message_content: content,
+            status: "SENT".to_string(),
+            error_message: "-".to_string(),
+            sent_at: Utc::now(),
+            created_at: Utc::now(),
+        })
+        .await?;
 
         Ok(())
     }
@@ -43,5 +62,31 @@ impl NotificationHandler {
             }
             _ => Err(anyhow::anyhow!("Unsupported channel")),
         }
+    }
+
+    async fn create_notification_log(
+        &self,
+        payload: &NotificationLog,
+    ) -> Result<bool, sqlx::Error> {
+        let rows = sqlx::query(
+            r#"
+                INSERT INTO notification_logs 
+                    (shipment_id, event_id, channel, 
+                     recipient_to, message_content, status,
+                     error_message, sent_at)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, NOW())
+                "#,
+        )
+        .bind(payload.shipment_id)
+        .bind(payload.event_id)
+        .bind(payload.channel.clone())
+        .bind(payload.recipient_to.clone())
+        .bind(payload.message_content.clone())
+        .bind(payload.status.clone())
+        .bind(payload.error_message.clone())
+        .execute(&self.pool)
+        .await?;
+
+        Ok(rows.rows_affected() == 1)
     }
 }
