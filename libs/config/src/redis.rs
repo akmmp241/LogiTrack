3ab8a395bd2b6d94f1f1b02f240612889ef07098
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2026-03-19 15:00:36 +0700

    feat(libs): add redis connection pool

--- libs/config/src/redis.rs
+++ libs/config/src/redis.rs
@@ -1,3 +1,4 @@
+use deadpool_redis::Runtime;
 use redis::Client;
 
 pub fn get_redis_client() -> Result<Client, redis::RedisError> {
@@ -7,4 +8,13 @@ pub fn get_redis_client() -> Result<Client, redis::RedisError> {
 
     tracing::info!("Redis client created");
     Ok(client)
+}
+
+pub async fn create_redis_pool() -> deadpool_redis::Pool {
+    let url = std::env::var("REDIS_URL").expect("REDIS_URL not set");
+
+    let cfg = deadpool_redis::Config::from_url(&url);
+
+    cfg.create_pool(Some(Runtime::Tokio1))
+        .expect("Cannot create redis pool")
 }
